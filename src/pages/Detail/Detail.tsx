0d81bbd90f73bc83a0c589bbc0ea85fd37abb0b0
Karen Sarria <karen.dani16@gmail.com>
2023-06-10 20:33:37 -0500

    chore: detail was created

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,4 +1,66 @@
+import { useSelector } from 'react-redux';
+import { changeSelection } from '../../reducer/character.selector';
+import s from './Detail.module.scss';
 const Detail = () => {
-  return <div>detail</div>;
+  const currentCharacter = useSelector(changeSelection);
+  if (currentCharacter) {
+    return (
+      <div className={s.content}>
+        <img className={s.image} src={currentCharacter.image} alt="character" />
+        <div className={s.info}>
+          <p>
+            <span>Name:</span>
+            <span> {currentCharacter.name}</span>
+          </p>
+          {currentCharacter.status && (
+            <p>
+              <span>Status:</span>
+              <span> {currentCharacter.status}</span>
+            </p>
+          )}
+          {currentCharacter.species && (
+            <p>
+              <span>Species:</span>
+              <span> {currentCharacter.species}</span>
+            </p>
+          )}
+          {currentCharacter.type && (
+            <p>
+              <span>Type:</span>
+              <span> {currentCharacter.type}</span>
+            </p>
+          )}
+          {currentCharacter.gender && (
+            <p>
+              <span>Gender:</span>
+              <span> {currentCharacter.gender}</span>
+            </p>
+          )}
+          {currentCharacter.location && (
+            <div>
+              {currentCharacter.location.name && (
+                <p>
+                  <span>Location:</span>
+                  <span> {currentCharacter.location.name}</span>
+                </p>
+              )}
+            </div>
+          )}
+          {currentCharacter.origin && (
+            <div>
+              {currentCharacter.origin.name && (
+                <p>
+                  <span>Origin:</span>
+                  <span> {currentCharacter.origin.name}</span>
+                </p>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  } else {
+    return <div>No Selected Character</div>;
+  }
 };
 export default Detail;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,37 +2,37 @@ import { useEffect, useState } from 'react';
 import { RickAndMortyService } from '../../services/RickAndMorty.Service';
 import { Character, Info } from '../../models/interfaces/character.interface';
 import Card from '../../components/Card/Card';
-import s from './Home.module.scss';
+
 import PaginationComp from '../../components/Pagination/Pagination';
 import Search from '../../components/Search/Search';
 import { useAppDispatch } from '../../app/hooks';
 import { useSelector } from 'react-redux';
 import { lastSelected } from '../../reducer/character.selector';
-import { insert } from '../../reducer/characters.actions';
+import { insert, currentSelection } from '../../reducer/characters.actions';
+import LastSelected from '../../components/LastSelected/LastSelected';
+import s from './Home.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const [characters, setCharacters] = useState<Character[]>();
   const [page, setPage] = useState<number>(1);
   const [info, setInfo] = useState<Info>();
   const [error, setError] = useState('');
-  const { getAll, filter } = RickAndMortyService;
+  const [name, setName] = useState('');
+  const { getAll } = RickAndMortyService;
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const lastSelectedList = useSelector(lastSelected);
-  useEffect(() => {
-    getAll(page)
-      .then((res) => {
-        setCharacters(res.results);
-        setInfo(res.info);
-      })
-      .catch((e) => setError(e.response.data.error));
-  }, [page, getAll]);
 
-  const changeName = (name: string) => {
-    filter(name)
+  useEffect(() => {
+    callService(1);
+  }, []);
+
+  const callService = (page: number, name?: string) => {
+    getAll(page, name)
       .then((res) => {
         setCharacters(res.results);
         setInfo(res.info);
-        setError('');
       })
       .catch((e) => setError(e.response.data.error));
   };
@@ -44,23 +44,32 @@ const Home = () => {
     }
     list.unshift(character);
     dispatch(insert(list));
+    dispatch(currentSelection(character));
+    navigate('/detail');
   };
   return (
     <>
-      <Search submit={(e) => changeName(e)}></Search>
+      <Search
+        submit={(e) => {
+          setName(e);
+          callService(1, e);
+        }}></Search>
       {error ? (
         <div className={s.error}> {error}</div>
       ) : (
         <>
-          {lastSelectedList?.length > 0 &&
-            lastSelectedList.map((selected) => <div>{selected.name}</div>)}
           {info && (
             <div className={s.pagination}>
               <PaginationComp
                 total={info.count}
                 defaultPageSize={20}
                 showSizeChanger={false}
-                change={(e: number) => setPage(e)}
+                current={page}
+                change={(e: number) => {
+                  setPage(e);
+                  console.log(name, e);
+                  callService(e, name);
+                }}
               />
             </div>
           )}
@@ -78,6 +87,7 @@ const Home = () => {
           </div>
         </>
       )}
+      <LastSelected />
     </>
   );
 };
